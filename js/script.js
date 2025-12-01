0a84f6f08788975ca7bf0e0d89885a342b70e721
rudyravelindev <rudyravelindev@gmail.com>
2025-12-01 14:58:31 -0800

    creating the constructor book

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log('Test OK now');
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+  this.id = crypto.randomUUID();
+}
+// Create first book
+const book1 = new Book('Atomic Habits', 'James Clear', 300, true);
+
+// Create second book
+const book2 = new Book('Clean Code', 'Robert C. Martin', 450, false);
+
+// Create third book
+const book3 = new Book(
+  'The Pragmatic Programmer',
+  'Andy Hunt & Dave Thomas',
+  320,
+  true
+);
+console.log(book1);
+console.log(book2);
+console.log(book3);
+
+// Step 2
